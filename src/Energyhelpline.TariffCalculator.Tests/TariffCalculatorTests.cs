24927281052e86ed3b743de4a2e40fc30c0a672d
Nicolas Markos <nikolaosmarkos@tfl.gov.uk>
2017-08-25 16:11:31 +0100

    added relative days and changed values to float

--- src/Energyhelpline.TariffCalculator.Tests/TariffCalculatorTests.cs
+++ src/Energyhelpline.TariffCalculator.Tests/TariffCalculatorTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Energyhelpline.TariffCalculator.Tests
@@ -14,18 +11,21 @@ namespace Energyhelpline.TariffCalculator.Tests
         {
         }
 
-        [TestCase(0.25, 0.5, 0.30, 0.60, 01/01/2018)]
-        public void EnergySaver_should_calculate_correct_tariff(decimal initialGasRate, decimal finalGasRate, decimal initialElectricityRate, decimal finalElectricityRate, DateTime expirationDate, decimal expectedResult)
+        //[TestCase(0.2, 0.4, 0.20, 0.40, "01/01/2018", 100)]
+        [TestCase(0.25f, 0.5f, 0.30f, 0.60f, "01/01/2018", 2015.55f)]
+        public void EnergySaver_should_calculate_correct_tariff(float initialGasRate, float finalGasRate, float initialElectricityRate, float finalElectricityRate, DateTime expirationDate, float expectedResult)
         {
+            const int daysPerYear = 365;
             //arrange
-            var daysBeforeExpiration = 0; //TODO
-            var daysAfterExpiration = 0;//TODO
+            var daysBeforeExpiration = (int)(expirationDate - DateTime.Today).TotalDays;
+            var daysAfterExpiration = 365 - daysBeforeExpiration;
+
+            var initialGasCost = daysBeforeExpiration / (float)daysPerYear * initialGasRate;
+            var finalGasCost = daysAfterExpiration / (float)daysPerYear * finalGasRate;
+            var initialElectricityCost = daysBeforeExpiration / (float)daysPerYear * initialElectricityRate;
+            var finalElectricityCost = daysAfterExpiration / (float)daysPerYear * finalElectricityRate;
 
             //act
-            var initialGasCost = daysBeforeExpiration * initialGasRate;
-            var finalGasCost = daysAfterExpiration * finalGasRate;
-            var initialElectricityCost = daysBeforeExpiration * initialElectricityRate;
-            var finalElectricityCost = daysAfterExpiration * finalElectricityRate;
             var annualCost = initialGasCost + finalGasCost + initialElectricityCost + finalElectricityCost;
 
             //assert
